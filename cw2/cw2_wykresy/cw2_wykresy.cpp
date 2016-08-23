f601a3af01864765ee5af6eee7d4bc259b498a6e
Maciej Michalec <maciej.michalec@gmail.com>
2016-08-23 23:22:28 +0200

    Drawing second histogram on the canvas (18)

--- cw2/cw2_wykresy/cw2_wykresy.cpp
+++ cw2/cw2_wykresy/cw2_wykresy.cpp
@@ -14,11 +14,16 @@ int main()
     TH1F *h_m_cond;
     TH1F *h_x1_cond;
 
+    TH1F *h_m_2;
+
     TFile *file = new TFile("../histogramy1.root", "READ");
     file->GetObject("h_m", h_m);
     file->GetObject("h_x1", h_x1);
     file->GetObject("h_m_cond", h_m_cond);
     file->GetObject("h_x1_cond", h_x1_cond);
+
+    TFile *file2 = new TFile("../histogramy2.root", "READ");
+    file2->GetObject("h_m", h_m_2);
 
     TCanvas *canvas = new TCanvas();
     canvas->Clear();
@@ -29,10 +34,14 @@ int main()
     TF1 *fit1 = (TF1*)h_m->GetFunction("gaus");
     fit1->Draw("same");
 
+    h_m_2->SetLineColor(kGreen);
+    h_m_2->Draw("same");
+
     canvas->SaveAs("histogramy.eps");
     canvas->SaveAs("histogramy.jpg");
 
     file->Close();
+    file2->Close();
     return 0;
 }
 

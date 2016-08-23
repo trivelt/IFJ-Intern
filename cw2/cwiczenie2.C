62f03c68738bfd60073f9b2015bef0223f6df9ef
Maciej Michalec <maciej.michalec@gmail.com>
2016-08-23 12:58:12 +0200

    Printing number of entries (5)

--- cw2/cwiczenie2.C
+++ cw2/cwiczenie2.C
@@ -3,12 +3,15 @@
 #include <TH2.h>
 #include <TStyle.h>
 #include <TCanvas.h>
+#include <iostream>
+using namespace std;
 
 void cwiczenie2::Loop()
 {
    if (fChain == 0) return;
 
    Long64_t nentries = fChain->GetEntriesFast();
+   cout << "Liczba przypadkow:\t" << nentries << endl;
 
    Long64_t nbytes = 0, nb = 0;
    for (Long64_t jentry=0; jentry<nentries;jentry++) {

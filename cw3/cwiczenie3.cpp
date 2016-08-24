6bf9d040195fcf6f335b2ea5d2afd9105194a814
Maciej Michalec <maciej.michalec@gmail.com>
2016-08-24 10:54:24 +0200

    Start of cwiczenie3 (1-2)

---
+++ cw3/cwiczenie3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <TROOT.h>
+
+using namespace std;
+
+double G_x(double x)
+{
+
+}
+
+double Q_x(double x)
+{
+
+}
+
+double inwersja_G(double u)
+{
+
+}
+
+double inwersja_Q(double u)
+{
+
+}
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+

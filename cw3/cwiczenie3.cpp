8ffa140eacdf2c49f519351dcc783c2b0588d6c5
Maciej Michalec <maciej.michalec@gmail.com>
2016-08-24 11:05:32 +0200

    Implemented G_x and Q_x methods (3.a,b)

--- cw3/cwiczenie3.cpp
+++ cw3/cwiczenie3.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-#include <TROOT.h>
+#include <TMath.h>
 
 using namespace std;
 
 double G_x(double x)
 {
-
+    double exp = 5/4.0;
+    return 5*x - 4*TMath::Power(x, exp);
 }
 
 double Q_x(double x)
 {
-
+    return 5*TMath::Power((x-1), 6) + 6*TMath::Power((x-1), 5) + 1;
 }
 
 double inwersja_G(double u)
@@ -25,7 +26,7 @@ double inwersja_Q(double u)
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << "x=3, G_x=" << Q_x(4) << endl;
     return 0;
 }
 

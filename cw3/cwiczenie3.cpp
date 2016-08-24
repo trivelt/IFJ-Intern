4b155c0d5bd317036169e216982dee24a654ae80
Maciej Michalec <maciej.michalec@gmail.com>
2016-08-24 11:57:17 +0200

    Implemented generating custom distributions (3.d, 4)

--- cw3/cwiczenie3.cpp
+++ cw3/cwiczenie3.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <TMath.h>
+#include <TRandom.h>
+#include <TH1F.h>
+#include <TCanvas.h>
 //#define DEBUG
 
 using namespace std;
@@ -48,9 +51,53 @@ double inwersja_Q(double u)
     return inwersja(Q_x, u);
 }
 
+double* custom_distribution( double(*inwersja)(double), int n)
+{
+    TRandom random(0);
+    double* array = new double[n];
+    for(int i=0; i<n; i++)
+    {
+        double randNumber = inwersja(random.Rndm());
+        array[i] = randNumber;
+    }
+    return array;
+}
+
+double* G_distribution(int n)
+{
+    return custom_distribution(inwersja_G, n);
+}
+
+double* Q_distribution(int n)
+{
+    return custom_distribution(inwersja_Q, n);
+}
+
 int main()
 {
-    cout << "x=4, G_x=" << G_x(0.0024) << ", inwersja_G=" << inwersja_G(0.0024) << endl;
+    TH1F *h_G = new TH1F("h_G", "h_G", 1000, 0, 1);
+    TH1F *h_Q = new TH1F("h_Q", "h_Q", 1000, 0, 0);
+
+//    cout << "x=4, G_x=" << G_x(0.0024) << ", inwersja_G=" << inwersja_G(0.0024) << endl;
+
+    int n = 1000000;
+    double* G_dist = G_distribution(n);
+    double* Q_dist = Q_distribution(n);
+    for(int i=0; i<n; i++)
+    {
+        h_G->Fill(G_dist[i]);
+        h_Q->Fill(Q_dist[i]);
+    }
+
+    TCanvas* canvas = new TCanvas();
+    canvas->Clear();
+    h_G->SetStats(kFALSE);
+    h_G->Draw();
+
+    h_Q->SetLineColor(kRed);
+    h_Q->Draw("same");
+    canvas->SaveAs("histG_Q.jpg");
+
     return 0;
 }
 
